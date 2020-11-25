ec04a60cc001c610321781f85cff8b38776f997a
Vafa Dehghan <v.dehghansaei@gmail.com>
2020-11-25 15:35:12 -0800

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var rectsRouter = require('./routes/rects');
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 var app = express();
 
 // view engine setup
