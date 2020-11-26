02f445c90e82a48c5eee15b2322ca1bf4283a1df
Vafa Dehghan <v.dehghansaei@gmail.com>
2020-11-25 17:19:21 -0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router();
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
+  host: "asn3-4-db",
+  user: "TESTUSER",
   password: "cmpt470",
   database: "cmpt470"
 });
